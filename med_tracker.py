1603a765e46f89172afd16eccba1db605ad429eb
altcode255 <anonte3p5usu@gmail.com>
2023-04-30 21:28:54 -0700

    Convert tabs to spaces

--- med_tracker.py
+++ med_tracker.py
@@ -6,95 +6,95 @@ import json
 
 
 def loadConfig(file_path):
-	with open(file_path, "r") as f:
-		return json.load(f)
+    with open(file_path, "r") as f:
+        return json.load(f)
 
 
 def createTable(db: str):
-	sqliteConnection = sqlite3.connect(db)
-	cursor = sqliteConnection.cursor()
-	cursor.execute("CREATE TABLE IF NOT EXISTS Med_Log (id INTEGER PRIMARY KEY, date TEXT, time TEXT)")
-	sqliteConnection.commit()
-	cursor.close()
-	sqliteConnection.close()
+    sqliteConnection = sqlite3.connect(db)
+    cursor = sqliteConnection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS Med_Log (id INTEGER PRIMARY KEY, date TEXT, time TEXT)")
+    sqliteConnection.commit()
+    cursor.close()
+    sqliteConnection.close()
 
 
 def getLog(records: int, db: str) -> str:
-	sqliteConnection = sqlite3.connect(db)
-	cursor = sqliteConnection.cursor()
-	cursor.execute(f"SELECT date, time FROM Med_Log ORDER BY id DESC LIMIT {records}")
-	output_list = [" - ".join(result) for result in cursor.fetchall()]
-	output_list.reverse()
-	cursor.close()
-	sqliteConnection.close()
-	return "\n".join(output_list)
+    sqliteConnection = sqlite3.connect(db)
+    cursor = sqliteConnection.cursor()
+    cursor.execute(f"SELECT date, time FROM Med_Log ORDER BY id DESC LIMIT {records}")
+    output_list = [" - ".join(result) for result in cursor.fetchall()]
+    output_list.reverse()
+    cursor.close()
+    sqliteConnection.close()
+    return "\n".join(output_list)
 
 
 def getLastDose(db: str) -> datetime:
-	sqliteConnection = sqlite3.connect(db)
-	cursor = sqliteConnection.cursor()
-	cursor.execute(f"SELECT date, time FROM Med_Log ORDER BY id DESC LIMIT 1")
-	last_dose = [" - ".join(result) for result in cursor.fetchall()]
-	cursor.close()
-	sqliteConnection.close()
-	dt_last_dose = datetime.strptime(last_dose[0], "%m/%d/%Y - %I:%M%p") if len(last_dose) > 0 else datetime.now()
-	return dt_last_dose
+    sqliteConnection = sqlite3.connect(db)
+    cursor = sqliteConnection.cursor()
+    cursor.execute(f"SELECT date, time FROM Med_Log ORDER BY id DESC LIMIT 1")
+    last_dose = [" - ".join(result) for result in cursor.fetchall()]
+    cursor.close()
+    sqliteConnection.close()
+    dt_last_dose = datetime.strptime(last_dose[0], "%m/%d/%Y - %I:%M%p") if len(last_dose) > 0 else datetime.now()
+    return dt_last_dose
 
 
 def getTimeSinceLastDose(last_dose: datetime) -> relativedelta:
-	return relativedelta.relativedelta(datetime.now(), last_dose)
+    return relativedelta.relativedelta(datetime.now(), last_dose)
 
 
 def doTally(time: str, db: str, max: int):
-	today = datetime.now().strftime("%m/%d/%Y")
-	sqliteConnection = sqlite3.connect(db)
-	cursor = sqliteConnection.cursor()
-	cursor.execute(f"SELECT time FROM Med_Log WHERE date='{today}'")
-	output_list = ["".join(result) for result in cursor.fetchall()]
-	if len(output_list) < max:
-		cursor.execute(f"INSERT INTO Med_Log (date, time) VALUES ('{today}', '{time}')")
-	sqliteConnection.commit()
-	cursor.close()
-	sqliteConnection.close()
+    today = datetime.now().strftime("%m/%d/%Y")
+    sqliteConnection = sqlite3.connect(db)
+    cursor = sqliteConnection.cursor()
+    cursor.execute(f"SELECT time FROM Med_Log WHERE date='{today}'")
+    output_list = ["".join(result) for result in cursor.fetchall()]
+    if len(output_list) < max:
+        cursor.execute(f"INSERT INTO Med_Log (date, time) VALUES ('{today}', '{time}')")
+    sqliteConnection.commit()
+    cursor.close()
+    sqliteConnection.close()
 
 
 def undoTally(db):
-	sqliteConnection = sqlite3.connect(db)
-	cursor = sqliteConnection.cursor()
-	cursor.execute(
-		f"DELETE FROM Med_Log where id IN (SELECT id FROM Med_Log ORDER BY id DESC LIMIT 1)"
-	)
-	sqliteConnection.commit()
-	cursor.close()
-	sqliteConnection.close()
+    sqliteConnection = sqlite3.connect(db)
+    cursor = sqliteConnection.cursor()
+    cursor.execute(
+        f"DELETE FROM Med_Log where id IN (SELECT id FROM Med_Log ORDER BY id DESC LIMIT 1)"
+    )
+    sqliteConnection.commit()
+    cursor.close()
+    sqliteConnection.close()
 
 
 if __name__ == "__main__":
-	config = loadConfig("config.json")
-	max_daily = config["max_daily_dose"] if type(config["max_daily_dose"]) is int else 2
-	db_name = config["database_name"] + ".db"
-	createTable(db_name)
-	
-	if len(sys.argv) < 2:
-		time = datetime.now().strftime("%I:%M%p")
-		doTally(time, db_name, max_daily)
-		print("Logged! Time is {time}.")
-	elif sys.argv[1] == "undo":
-		undoTally(db_name)
-		print("Done! Previous entry has been undone.")
-	elif sys.argv[1] == "log":
-		if len(sys.argv) < 3 or not sys.argv[2].isdigit:
-			n = max_daily
-		else:
-			n = int(sys.argv[2])
-		time_diff = getTimeSinceLastDose(getLastDose(db_name))
-		last_dose_msg = f"Last dose was {time_diff.days} day{'' if time_diff.days == 1 else 's'}, {time_diff.hours} hour{'' if time_diff.hours == 1 else 's'}, and {time_diff.minutes} minute{'' if time_diff.minutes == 1 else 's'} ago."
-		print("Medication Log\n---\n" + getLog(n, db_name) + "\n---\n" + last_dose_msg)
-	else:
-		try:
-			datetime.strptime(sys.argv[1], "%I:%M%p")
-			time = sys.argv[1]
-			doTally(time, db_name, max_daily)
-			print(f"Logged! Time is {time}.")
-		except ValueError:
-			print("Invalid time! Please use HH:MM(AM/PM) format. Ex. 05:13PM", file=sys.stderr)
+    config = loadConfig("config.json")
+    max_daily = config["max_daily_dose"] if type(config["max_daily_dose"]) is int else 2
+    db_name = config["database_name"] + ".db"
+    createTable(db_name)
+    
+    if len(sys.argv) < 2:
+        time = datetime.now().strftime("%I:%M%p")
+        doTally(time, db_name, max_daily)
+        print("Logged! Time is {time}.")
+    elif sys.argv[1] == "undo":
+        undoTally(db_name)
+        print("Done! Previous entry has been undone.")
+    elif sys.argv[1] == "log":
+        if len(sys.argv) < 3 or not sys.argv[2].isdigit:
+            n = max_daily
+        else:
+            n = int(sys.argv[2])
+        time_diff = getTimeSinceLastDose(getLastDose(db_name))
+        last_dose_msg = f"Last dose was {time_diff.days} day{'' if time_diff.days == 1 else 's'}, {time_diff.hours} hour{'' if time_diff.hours == 1 else 's'}, and {time_diff.minutes} minute{'' if time_diff.minutes == 1 else 's'} ago."
+        print("Medication Log\n---\n" + getLog(n, db_name) + "\n---\n" + last_dose_msg)
+    else:
+        try:
+            datetime.strptime(sys.argv[1], "%I:%M%p")
+            time = sys.argv[1]
+            doTally(time, db_name, max_daily)
+            print(f"Logged! Time is {time}.")
+        except ValueError:
+            print("Invalid time! Please use HH:MM(AM/PM) format. Ex. 05:13PM", file=sys.stderr)
--- med_tracker_GUI.py
+++ med_tracker_GUI.py
@@ -4,47 +4,47 @@ from datetime import datetime
 
 
 def getLastDoseMsg(db: str, label: Label):
-	time_diff = getTimeSinceLastDose(getLastDose(db))
-	last_dose_msg = f"Last dose was {time_diff.days} day{'' if time_diff.days == 1 else 's'}, {time_diff.hours} hour{'' if time_diff.hours == 1 else 's'}, and {time_diff.minutes} minute{'' if time_diff.minutes == 1 else 's'} ago."
-	label.config(text=last_dose_msg)
-	label.grid(row=0, column=0, columnspan=2, pady=5)
-	window.after(30000, lambda: getLastDoseMsg(db, label))
+    time_diff = getTimeSinceLastDose(getLastDose(db))
+    last_dose_msg = f"Last dose was {time_diff.days} day{'' if time_diff.days == 1 else 's'}, {time_diff.hours} hour{'' if time_diff.hours == 1 else 's'}, and {time_diff.minutes} minute{'' if time_diff.minutes == 1 else 's'} ago."
+    label.config(text=last_dose_msg)
+    label.grid(row=0, column=0, columnspan=2, pady=5)
+    window.after(30000, lambda: getLastDoseMsg(db, label))
 
 
 def getLogMsg(db: str, label: Label):
-	label.config(text="Dose Log (last 10 entries):\n" + getLog(10, db))
-	label.grid(row=3, column=0, columnspan=2, pady=15)
-	window.after(30000, lambda: getLogMsg(db, label))
+    label.config(text="Dose Log (last 10 entries):\n" + getLog(10, db))
+    label.grid(row=3, column=0, columnspan=2, pady=15)
+    window.after(30000, lambda: getLogMsg(db, label))
 
 
 def updateMsgs(db: str):
-	log.config(text="Dose Log (last 10 entries):\n" + getLog(10, db))
-	log.grid(row=3, column=0, columnspan=2, pady=15)
-	time_diff = getTimeSinceLastDose(getLastDose(db))
-	last_dose_msg = f"Last dose was {time_diff.days} day{'' if time_diff.days == 1 else 's'}, {time_diff.hours} hour{'' if time_diff.hours == 1 else 's'}, and {time_diff.minutes} minute{'' if time_diff.minutes == 1 else 's'} ago."
-	last_dose.config(text=last_dose_msg)
-	last_dose.grid(row=0, column=0, columnspan=2, pady=5)
+    log.config(text="Dose Log (last 10 entries):\n" + getLog(10, db))
+    log.grid(row=3, column=0, columnspan=2, pady=15)
+    time_diff = getTimeSinceLastDose(getLastDose(db))
+    last_dose_msg = f"Last dose was {time_diff.days} day{'' if time_diff.days == 1 else 's'}, {time_diff.hours} hour{'' if time_diff.hours == 1 else 's'}, and {time_diff.minutes} minute{'' if time_diff.minutes == 1 else 's'} ago."
+    last_dose.config(text=last_dose_msg)
+    last_dose.grid(row=0, column=0, columnspan=2, pady=5)
 
 
 def grabTime(input: Entry, db: str, max: int):
-	time = input.get()
-	try:
-		datetime.strptime(time, "%I:%M%p")
-		doTally(time, db_name, max)
-		input.delete(0, END)
-		updateMsgs(db)
-	except ValueError:
-		input.delete(0, END)
-		input.insert(0, "Please use HH:MM(AM/PM) format. Ex. 05:13PM")
+    time = input.get()
+    try:
+        datetime.strptime(time, "%I:%M%p")
+        doTally(time, db_name, max)
+        input.delete(0, END)
+        updateMsgs(db)
+    except ValueError:
+        input.delete(0, END)
+        input.insert(0, "Please use HH:MM(AM/PM) format. Ex. 05:13PM")
 
 
 def undo(db: str):
-	undoTally(db)
-	updateMsgs(db)
+    undoTally(db)
+    updateMsgs(db)
 
 def record(db: str, max: int):
-	doTally(datetime.now().strftime("%I:%M%p"), db, max)
-	updateMsgs(db)
+    doTally(datetime.now().strftime("%I:%M%p"), db, max)
+    updateMsgs(db)
 
 config = loadConfig("config.json")
 max_daily = config["max_daily_dose"] if type(config["max_daily_dose"]) is int else 2
